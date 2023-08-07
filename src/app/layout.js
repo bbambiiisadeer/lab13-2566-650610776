f3ff71dc4f2fe7b542d4f31a8c6e85e04f1bad2e
bbam <nunnapats1822@gmail.com>
2023-08-07 16:00:26 +0700

    completed

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,8 +28,8 @@ export default function RootLayout({ children }) {
           {children}
           <Footer
             year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            fullName="Nunnapat Sirithanachokpaisan"
+            studentId="650610776"
           />
         </div>
       </body>
--- src/app/search/[searchInput]/page.js
+++ src/app/search/[searchInput]/page.js
@@ -3,28 +3,29 @@
 import { MovieRow } from "@/components/MovieRow";
 import { movieDB } from "@/libs/movieDB";
 
-export default function SearchResultPage() {
-  //tip1 : before filtering movie, replace all "%20" with " " (space) in the input
-  // const processedSearchInput = ...
-
-  /*
-  tip2 : Use "includes" string method to check substring
-  Example : "ABC".includes("AB") -> return true
-
-  tip3 : To implement case insensitive searching, use "toLocaleLowerCase" string method
-  to convert movie title and searchInput to lower case 
+export default function SearchResultPage({params}) {
+  const processedSearchInput = params.searchInput.replaceAll("%20", " ")
   const filteredMovies = movieDB.filter((movie) =>
-    you code here...
+    movie.title.toLowerCase().includes(processedSearchInput.toLowerCase())
   );
-  */
 
   return (
     <div>
       <p className="fw-bold fs-4 text-center my-0">
-        Searching &quot; ... &quot;
+        Searching "{processedSearchInput}"
       </p>
-      <p className="fw-bold fs-4 text-center">Found ... result(s)</p>
-      {/* Use  "filteredMovies" variable to map-loop rendering MovieRow component */}
+      <p className="fw-bold fs-4 text-center">Found {filteredMovies.length} result(s)</p>
+      {
+        filteredMovies.map((movie, i) =>(
+        <MovieRow
+            key={movie.id}
+            id={movie.id}
+            title={movie.title}
+            detail={movie.detail}
+            rating={movie.rating}
+            number={i+1}
+        />
+    ))}
     </div>
   );
 }
